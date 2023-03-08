02235a9a819878177c677eb18fc61430d04a7fea
RobersonBorges <robersonfborges@gmail.com>
2023-03-08 18:23:46 -0300

    trabalhando com numeros, básico

---
+++ ProjetoJava2/src/exercicios/curso/CalculandoValores.java
@@ -0,0 +1,15 @@
+package exercicios.curso;
+
+public class CalculandoValores {
+
+
+public static void main(String[]args) {
+	int nota1 = 90;
+	int nota2 = 80;
+	int nota3 = 70;
+	double media = (nota1+nota2+nota3) /4;
+	System.out.println("a soma das notas é: " + (nota1+nota2+nota3));
+	System.out.println("a média das notas é: "+ media);
+	
+}
+}
